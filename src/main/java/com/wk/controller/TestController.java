05dc614f98fed7a819fdb7b25e09f0a2f4d2f1e0
wkgui <GUIWANKAI@KINGSOFT.COM>
2018-03-14 22:03:03 +0800

    log4j

--- src/main/java/com/wk/controller/TestController.java
+++ src/main/java/com/wk/controller/TestController.java
@@ -1,12 +1,18 @@
 package com.wk.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class TestController {
+	private static Logger logger = Logger.getLogger(TestController.class);
+
 	@GetMapping("/")
 	public String test(){
+		logger.info("ssssss");
+		logger.error("xxxxxx");
 		return "111111";
 	}
 }
