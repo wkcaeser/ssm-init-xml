c0a8bb1e9c232b9b6b50e67387d376b67bb627e8
wkgui <GUIWANKAI@KINGSOFT.COM>
2018-03-14 21:06:25 +0800

    ssm-init-xml

---
+++ src/main/java/com/wk/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.wk.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@GetMapping("/")
+	public String test(){
+		return "111111";
+	}
+}
